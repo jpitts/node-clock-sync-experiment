d20059d38cb2c5ab7f12fc7ff98a667f70a0a169
jpitts <james.pitts@gmail.com>
2014-01-17 13:34:55 -0800

    Created a CSE module and env-based configs.
    - created a core Clock Sync Experiment module with an init method
    - added an env-based config approach with elegant fail

--- components/web_service/app.js
+++ components/web_service/app.js
@@ -7,59 +7,52 @@ var express = require('express')
 ;
 
 
-/*
-  configuration
-  TODO: replace with json files
-*/
 
-var service_cfg = {
-  web: {
-    host: 'localhost',
-    port: (process.env.CLOCK_WEB_PORT ? process.env.CLOCK_WEB_PORT : 8080),
-  },
-  websocket: {
-    host: 'localhost',
-    port: (process.env.CLOCK_WS_PORT ? process.env.CLOCK_WS_PORT : 8081),
-  },
-  clock: {
-    ticks_per_second: 1
-  }
-};
+// initialize the clock sync experiment module
+require('../../lib/ClockSyncExperiment').init({}, function (err, CSE) {
+
+  /*
+    configuration
+    NOTE: to load environment-based config, set NODE_ENV=dev in the shell script call
+  */
+  
+  var service_cfg = CSE.config;
 
 
-/*
-  define the service
-*/
+  /*
+    service definition
+  */
 
-var app = express();
+  var app = express();
 
-app.use( partials() ); /* 
-For rendering ejs views. 
-SEE: 
-  https://github.com/publicclass/express-partials
-  http://embeddedjs.com/
-*/
+  app.use( partials() ); /* 
+  For rendering ejs views. 
+  SEE: 
+    https://github.com/publicclass/express-partials
+    http://embeddedjs.com/
+  */
 
-app.use(express.static(__dirname + '/public')); /*
-For static files in the public directory
-*/
+  app.use(express.static(__dirname + '/public')); /*
+  For static files in the public directory
+  */
 
 
-/*
-  define the web routes
-*/
+  /*
+    define the web routes
+  */
 
-app.get('/', function(req, res){
-  res.render('index.ejs', { 
-    layout:false, 
-    service_cfg: service_cfg 
-  }); 
+  app.get('/', function(req, res){
+    res.render('index.ejs', { 
+      layout:false, 
+      service_cfg: service_cfg 
+    }); 
+  });
+
+
+  // finally, start listening
+  app.listen(service_cfg.web.port);
+  console.log('Web listening on port[' + service_cfg.web.port + '].');
+
+
 });
-
-
-// finally, start listening
-app.listen(service_cfg.web.port);
-console.log('Web listening on port[' + service_cfg.web.port + '].');
-
-
 
--- components/websocket_service/app.js
+++ components/websocket_service/app.js
@@ -5,125 +5,119 @@ var http = require('http')
   , sio = require('socket.io')
 ;
 
-/*
-   attributes
-*/
+// initialize the clock sync experiment module
+require('../../lib/ClockSyncExperiment').init({}, function (err, CSE) {
 
-// socket_pool - tracking all connected clients by socket id
-var socket_pool = {};
-
-
-/* 
-  configuration 
-  TODO: replace with json files
-*/
-
-var service_cfg = {
-  web: {
-    host: 'localhost',
-    port: (process.env.CLOCK_WEB_PORT ? process.env.CLOCK_WEB_PORT : 8080),
-  },
-  websocket: {
-    host: 'localhost',
-    port: (process.env.CLOCK_WS_PORT ? process.env.CLOCK_WS_PORT : 8081),
-  },
-  clock: {
-    ticks_per_second: 1
-  }
-};
-
-// service
-//var websocket_server = eio.listen( service_cfg.websocket.port );
-
-var websocket_server = sio.listen(service_cfg.websocket.port, {   
-  log: false 
-});
-
-console.log('WS listening on port[' + service_cfg.websocket.port + '].');
-
-
-// define the websocket listeners
-
-websocket_server.on('connection', function (socket) {
-  console.log('websocket connection established from id[' + socket.id + '].');  
-  
-  // store the socket for later use
-  socket_pool[socket.id] = socket; 
- 
-  // on message
-  socket.on('message', function () { 
-    console.log('on message');  
-    
-  });
-  
-  // on disconnect
-  socket.on('disconnect', function () { 
-    console.log('on disconnect');  
-    
-    // delete from the pool
-    delete socket_pool[socket.id];
-
-  });
-  
-
-  // custom listeners
-
-  socket.on('system.connect_ack', function (payload) {
-    console.log('connect_ack() at ' + payload.time_connected);
-  }); 
-
-  socket.on('clock.tick_ack', function (payload) {
-    console.log('on clock.tick_ack from client id[' + socket.id + '] at ' + payload.time_received);
-  });
-
-});
-
-
-// start listening
-sio.listen( service_cfg.websocket.port )
-
-
-// start ticking
-
-var start_ticking = function (attr) {
-  /* attrs:
-      socket
+  /*
+     attributes
   */
+
+  // socket_pool - tracking all connected clients by socket id
+  var socket_pool = {};
+
+
+  /* 
+    configuration 
+    NOTE: to load environment-based config, set NODE_ENV=dev in the shell script call
+  */
+
+  var service_cfg = CSE.config;
   
-  console.log('start_ticking()');  
 
-  var intervalId = setInterval(function(){
+  /* 
+    service definition
+  */
+
+  var websocket_server = sio.listen(service_cfg.websocket.port, {   
+    log: false 
+  });
+
+  console.log('WS listening on port[' + service_cfg.websocket.port + '].');
+
+
+  // define the websocket listeners
+
+  websocket_server.on('connection', function (socket) {
+    console.log('websocket connection established from id[' + socket.id + '].');  
     
-    var current_time = (new Date).getTime();
-    var tick_data = {
-      time: current_time,
-    };
-       
-    console.log('service tick ' + current_time);
-    
-    // check for connected clients
-    if (socket_pool && Object.keys(socket_pool).length) {
-      var socket_ids = Object.keys(socket_pool);
+    // store the socket for later use
+    socket_pool[socket.id] = socket; 
+   
+    // on message
+    socket.on('message', function () { 
+      console.log('on message');  
       
-      // broadcast tick to all clients
-      //console.log('broadcast to websockets clients: ', socket_ids);  
-       
-      for (var i=0; i<socket_ids.length; i++) {
+    });
+    
+    // on disconnect
+    socket.on('disconnect', function () { 
+      console.log('on disconnect');  
+      
+      // delete from the pool
+      delete socket_pool[socket.id];
+
+    });
+    
+
+    // custom listeners
+
+    socket.on('system.connect_ack', function (payload) {
+      console.log('connect_ack() at ' + payload.time_connected);
+    }); 
+
+    socket.on('clock.tick_ack', function (payload) {
+      console.log('on clock.tick_ack from client id[' + socket.id + '] at ' + payload.time_received);
+    });
+
+  });
+
+
+  // start listening
+  sio.listen( service_cfg.websocket.port )
+
+
+  // start ticking
+
+  var start_ticking = function (attr) {
+    /* attrs:
+        socket
+    */
+    
+    console.log('start_ticking()');  
+
+    var intervalId = setInterval(function(){
+      
+      var current_time = (new Date).getTime();
+      var tick_data = {
+        time: current_time,
+      };
+         
+      console.log('service tick ' + current_time);
+      
+      // check for connected clients
+      if (socket_pool && Object.keys(socket_pool).length) {
+        var socket_ids = Object.keys(socket_pool);
         
-        var client = socket_pool[ socket_ids[i] ];
-        console.log('emit clock.tick to client id[' + client.id + '].');
-        
-        // emit the tick to this client
-        client.emit('clock.tick', tick_data);  
-      }
-       
-    } 
- 
-  }, ( 1000 / (service_cfg.clock.ticks_per_second ? service_cfg.clock.ticks_per_second : 1) ) );
-}
+        // broadcast tick to all clients
+        //console.log('broadcast to websockets clients: ', socket_ids);  
+         
+        for (var i=0; i<socket_ids.length; i++) {
+          
+          var client = socket_pool[ socket_ids[i] ];
+          console.log('emit clock.tick to client id[' + client.id + '].');
+          
+          // emit the tick to this client
+          client.emit('clock.tick', tick_data);  
+        }
+         
+      } 
+   
+    }, ( 1000 / (service_cfg.clock.ticks_per_second ? service_cfg.clock.ticks_per_second : 1) ) );
+  }
 
 
-// start it
-start_ticking();
+  // start it
+  start_ticking();
 
+});
 
